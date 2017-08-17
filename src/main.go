c4c2087a9b0c11ecac32b5541a787105124d7690
Minh Chau <nmchau87@gmail.com>
2017-08-17 23:02:30 +0900

    style: remove empty lines

--- src/main.go
+++ src/main.go
@@ -15,21 +15,18 @@ import (
 func downloadFile(url string, dest string) (file string) {
 	tokens := strings.Split(url, "/")
 	filePath := filepath.Join(dest, tokens[len(tokens)-1])
-
 	output, err := os.Create(filePath)
 	if err != nil {
 		fmt.Println("Error while creating", filePath, "-", err)
 		return
 	}
 	defer output.Close()
-
 	response, err := http.Get(url)
 	if err != nil {
 		fmt.Println("Error while downloading", url, "-", err)
 		return
 	}
 	defer response.Body.Close()
-
 	if _, err := io.Copy(output, response.Body); err != nil {
 		fmt.Println("Error while downloading", url, "-", err)
 		return
@@ -42,35 +39,29 @@ func unzip(archive, target string) error {
 	if err != nil {
 		return err
 	}
-
 	if err := os.MkdirAll(target, 0755); err != nil {
 		return err
 	}
-
 	for _, file := range reader.File {
 		path := filepath.Join(target, file.Name)
 		if file.FileInfo().IsDir() {
 			os.MkdirAll(path, file.Mode())
 			continue
 		}
-
 		fileReader, err := file.Open()
 		if err != nil {
 			return err
 		}
 		defer fileReader.Close()
-
 		targetFile, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode())
 		if err != nil {
 			return err
 		}
 		defer targetFile.Close()
-
 		if _, err := io.Copy(targetFile, fileReader); err != nil {
 			return err
 		}
 	}
-
 	return nil
 }
 
@@ -99,7 +90,6 @@ func main() {
 	if homeDirInput != "" {
 		homeDir = homeDirInput
   }
-
   var version = "latest"
   fmt.Print("Version [", version, "]: ")
 	var versionInput string
@@ -107,15 +97,12 @@ func main() {
 	if versionInput != "" {
 		version = versionInput
 	}
-  
   var downloadUrl = strings.Replace(DownloadUrl, "{version}", version, -1)
   downloadUrl = strings.Replace(downloadUrl, "{env}", getTargetOS(), -1)
-
 	// Install
 	fmt.Println("Installing...")
   var file = downloadFile(downloadUrl, os.TempDir())
 	unzip(file, homeDir)
-	fmt.Println(file)
   os.Remove(file)
   fmt.Println("Finished")
 }
